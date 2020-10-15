9bc435411cf083131282cb7e4ae62b2385768c68
A.N <spidernet12@gmail.com>
2020-10-15 18:14:12 +0300

    Updates

---
+++ precache-manifest.da68a7112db9c5ff8d0e2285cc34e8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f78dcbfe827785c1d006ad014bb8b56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc1ff46d6efb07376896",
+    "url": "/static/css/main.f46a6f2b.chunk.css"
+  },
+  {
+    "revision": "26e9abef7d69706d5777",
+    "url": "/static/js/2.b50584e7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.b50584e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1ff46d6efb07376896",
+    "url": "/static/js/main.dfa45ae3.chunk.js"
+  },
+  {
+    "revision": "059c3803897603e8c07c",
+    "url": "/static/js/runtime-main.502d264b.js"
+  }
+]);
