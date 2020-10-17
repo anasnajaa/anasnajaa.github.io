f909e83590ac0058a23ea3260b2a4c7c8338b6d7
A.N <spidernet12@gmail.com>
2020-10-17 10:24:52 +0300

    added loading and. changed posts limit

--- src/components/BlogPosts/index.js
+++ src/components/BlogPosts/index.js
@@ -3,6 +3,7 @@ import { getPosts } from '../../api/blog';
 import Pagination from '../Pagination/index';
 import BlogPost from '../BlogPostSimple/index';
 import { Row, Col } from 'react-bootstrap';
+import Loading from '../Loading/index';
 class BlogPosts extends React.Component {
   constructor(props) {
     super(props);
@@ -17,7 +18,7 @@ class BlogPosts extends React.Component {
 
   async getBlogPosts() {
     try {
-      const response =  await getPosts(1, this.state.currentPage);
+      const response =  await getPosts(5, this.state.currentPage);
       console.log(response);
       this.setState({
         error: null,
@@ -51,23 +52,24 @@ class BlogPosts extends React.Component {
       return (
         <div>Error: {this.state.error.message}</div>
       );
+    } else if (!this.state.isLoaded) {
+      return (
+        <Row>
+          <Col className="text-center">
+            <Loading />
+          </Col>
+        </Row>
+      )
     } else {
       return (
-        <>
-        {!this.state.isLoaded ?
-        <div>Loading...</div>
-        :
         <Row>
           {this.state.posts.map(post => (
-            <Col key={post.id} xs={12} sm={12} md={12} lg={6} xl={6}>
+            <Col key={post.id} xs={12} sm={12} md={12} lg={12} xl={12}>
               <BlogPost post={post}/>
             </Col>
           ))}
-        </Row>
-        }
-        <Row>
           <Col xs={12} sm={12} md={4} lg={4} xl={4} />
-          <Col xs={12} sm={12} md={4} lg={4} xl={4} >
+          <Col className="text-center" xs={12} sm={12} md={4} lg={4} xl={4} >
             <Pagination 
               pg={this.state.pagination} 
               onNextClick={()=>{
@@ -84,7 +86,6 @@ class BlogPosts extends React.Component {
           </Col>
           <Col xs={12} sm={12} md={4} lg={4} xl={4} />
         </Row>
-        </>
       );
     }
   }  
