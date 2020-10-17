5ac566bf31f969e699c3f9e1bccb71fafe713549
A.N <spidernet12@gmail.com>
2020-10-17 10:24:18 +0300

    color resolver utility for tags

---
+++ src/components/TagsUtility/index.js
@@ -0,0 +1,14 @@
+export const TagsStyleResolver = (id) => {
+    if(id === "5f86bc611488fc001eea51ae"){
+        return {backgroundColor: "", color: "#16063A"};
+    }
+    if(id === "5f8a7e4a11e139001eb1d5d2"){
+        return {backgroundColor: "", color: "#2A0238"};
+    }
+    if(id === "5f7f519ecbab9f001ede0c9b"){
+        return {backgroundColor: "", color: "#4F5400"};
+    }
+    if(id === "5f8a7f6d11e139001eb1d5d6"){
+        return {backgroundColor: "", color: "003925"};
+    }
+}
