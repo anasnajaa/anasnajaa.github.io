d5845b6ec71d54cf231e2c51954e6029f06dab5a
A.N <spidernet12@gmail.com>
2020-10-17 16:51:43 +0300

    cleaned up page elements

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import {
-    Switch,
-    Route,
-    useRouteMatch
-  } from "react-router-dom";
+import React from "react";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 
-import BlogPostBySlug from '../BlogPostBySlug/index';
-import BlogPosts from '../BlogPosts/index';
-import { Container, Row, Col } from 'react-bootstrap';
+import BlogPostBySlug from "../BlogPostBySlug/index";
+import BlogPosts from "../BlogPosts/index";
+import { Container, Row, Col } from "react-bootstrap";
+import BlogPostsArchive from "../BlogPostsArchive/index";
+import BlogNav from '../BlogNav/index';
 
 function Blog() {
-    let { path } = useRouteMatch();
-  
-    return (
-      <div>  
-        <Switch>
-          <Route exact path={path}>
-            <Container>
-              <Row>
-                <Col xs={12} sm={12} md={12} lg={2} xl={2}/>
-                <Col xs={12} sm={12} md={12} lg={8} xl={8}>
-                  <BlogPosts/>
-                </Col>
-                <Col xs={12} sm={12} md={12} lg={2} xl={2}/>
-              </Row>
-            </Container>
-          </Route>
-          <Route path={`${path}/post/:slug`}>
-            <BlogPostBySlug />
-          </Route>
-        </Switch>
-      </div>
-    );
-  }
+  let { path } = useRouteMatch();
 
-  export default Blog;
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={2} xl={2} />
+        <Col>
+          <BlogNav/>
+          <Switch>
+            <Route path={`${path}/post/:slug`}>
+              <BlogPostBySlug />
+            </Route>
+            <Route path={`${path}/archive`}>
+              <BlogPostsArchive />
+            </Route>
+            <Route exact path={path}>
+              <BlogPosts />
+            </Route>
+          </Switch>
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={2} xl={2} />
+      </Row>
+    </Container>
+  );
+}
+
+export default Blog;
