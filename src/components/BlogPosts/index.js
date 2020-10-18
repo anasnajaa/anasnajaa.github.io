909c0c57d16d4a9f6e521c6f28dc11dc9e42d16e
A.N <spidernet12@gmail.com>
2020-10-18 18:20:44 +0300

    convereted to functional component

--- src/components/BlogPosts/index.js
+++ src/components/BlogPosts/index.js
@@ -1,97 +1,98 @@
-import React from 'react';
-import { getPosts } from '../../api/blog';
-import Pagination from '../Pagination/index';
-import BlogPost from '../BlogPostSimple/index';
-import { Row, Col } from 'react-bootstrap';
-import Loading from '../Loading/index';
-class BlogPosts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      posts: [],
-      pagination: null,
-      currentPage: 1
-    }
-  }
+import React, { useEffect, useState } from "react";
+import { getPosts } from "../../api/blog";
+import Pagination from "../Pagination/index";
+import BlogPost from "../BlogPostSimple/index";
+import { Row, Col } from "react-bootstrap";
+import Loading from "../Loading/index";
 
-  async getBlogPosts() {
+const BlogPosts = ({topic}) => {
+  const [topicTitle, setTopicTitle] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [pagination, setPagination] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const getBlogPosts = async (page, topic) => {
     try {
-      const response =  await getPosts(5, this.state.currentPage);
-      this.setState({
-        error: null,
-        isLoaded: true,
-        posts: response.posts,
-        pagination: response.meta.pagination
-      });
+      const response = await getPosts(5, page, topic);
+      setError(null);
+      setPosts(response.posts);
+      setPagination(response.meta.pagination);
+
+      if(topic){
+        const title = response.posts[0].tags.filter(x=>x.slug===topic)[0].name;
+        setTopicTitle(title);
+      }
+
     } catch (error) {
-      this.setState({
-        error: error,
-        isLoaded: true
-      });
+      setError(error);
+    } finally {
+      setIsLoaded(true);
     }
   };
 
-  updateCurrentPage(page){
-    this.setState({
-      currentPage: page,
-      isLoaded: false
-    }, ()=>{
-      this.getBlogPosts();
-    });
-  }
+  const updateCurrentPage = (page) => {
+    setCurrentPage(page);
+    setIsLoaded(false);
+  };
 
-  componentDidMount() {
-    this.getBlogPosts();
-  }
+  useEffect(()=>{
+    getBlogPosts(currentPage, topic);
+  }, [currentPage, topic]);
 
-  render() {
-    if (this.state.error) {
-      return (
-        <Row>
-          <Col className="text-center">
-          <div>Error: {this.state.error.message}</div>
+  if (error) {
+    return (
+      <Row>
+        <Col className="text-center">
+          <div>Error: {error.message}</div>
+        </Col>
+      </Row>
+    );
+  } else if (!isLoaded) {
+    return (
+      <Row>
+        {topic &&
+        <Col xs={12}>
+          <h3><i>{topicTitle}</i></h3>
+        </Col>}
+        <Col className="text-center">
+          <Loading />
+        </Col>
+      </Row>
+    );
+  } else {
+    return (
+      <Row>
+        {topic &&
+        <Col xs={12}>
+          <h3><i>{topicTitle}</i></h3>
+        </Col>}
+        {posts.map((post) => (
+          <Col key={post.id} xs={12} sm={12} md={12} lg={12} xl={12}>
+            <BlogPost post={post} />
           </Col>
-        </Row>
-      );
-    } else if (!this.state.isLoaded) {
-      return (
-        <Row>
-          <Col className="text-center">
-            <Loading />
-          </Col>
-        </Row>
-      )
-    } else {
-      return (
-        <Row>
-          {this.state.posts.map(post => (
-            <Col key={post.id} xs={12} sm={12} md={12} lg={12} xl={12}>
-              <BlogPost post={post}/>
-            </Col>
-          ))}
-          <Col xs={12} sm={12} md={4} lg={4} xl={4} />
-          <Col className="text-center" xs={12} sm={12} md={4} lg={4} xl={4} >
-            <Pagination 
-              pg={this.state.pagination} 
-              onNextClick={()=>{
-                this.updateCurrentPage(this.state.currentPage + 1);
-              }} 
-              onPreviousClick={()=>{
-                this.updateCurrentPage(this.state.currentPage - 1);
-              }} 
-              onPageClick={(selectPageNumber) => {
-                this.updateCurrentPage(selectPageNumber);
-              }}
-              isLoading={!this.state.isLoaded}
-            />
-          </Col>
-          <Col xs={12} sm={12} md={4} lg={4} xl={4} />
-        </Row>
-      );
-    }
-  }  
+        ))}
+        <Col xs={12} sm={12} md={4} lg={4} xl={4} />
+        <Col className="text-center" xs={12} sm={12} md={4} lg={4} xl={4}>
+          <Pagination
+            pg={pagination}
+            onNextClick={() => {
+              updateCurrentPage(currentPage + 1);
+            }}
+            onPreviousClick={() => {
+              updateCurrentPage(currentPage - 1);
+            }}
+            onPageClick={(selectPageNumber) => {
+              updateCurrentPage(selectPageNumber);
+            }}
+            isLoading={!isLoaded}
+          />
+        </Col>
+        <Col xs={12} sm={12} md={4} lg={4} xl={4} />
+      </Row>
+    );
+  }
 };
 
 export default BlogPosts;
