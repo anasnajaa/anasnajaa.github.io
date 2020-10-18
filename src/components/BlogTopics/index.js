060ef45f5254d3fbad61941527145966b1d3e685
A.N <spidernet12@gmail.com>
2020-10-18 18:21:42 +0300

    intial tags logic added

---
+++ src/components/BlogTopics/index.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { ListGroup, Row, Col, Badge } from "react-bootstrap";
+import { getTags } from "../../api/blog";
+import Loading from "../Loading/index";
+import { LinkContainer } from "react-router-bootstrap";
+import { v4 } from "uuid";
+
+const BlogTopics = () => {
+  const [tags, setTags] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+
+  const populateTags = async () => {
+    try {
+      const response = await getTags();
+      setTags(response.tags);
+      console.log(response);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    populateTags();
+  }, []);
+
+  if (error) {
+    return (
+      <Row>
+        <Col>Error: {error}</Col>
+      </Row>
+    );
+  }
+
+  if (!isLoaded) {
+    return (
+      <Row>
+        <Col className="text-center">
+          <Loading />
+        </Col>
+      </Row>
+    );
+  }
+
+  return (
+    <Row>
+      <Col>
+        <ListGroup>
+          {tags.map(({ id, description, name, slug, count }) => {
+            return (
+              <LinkContainer
+                className="cursor-pointer"
+                key={id}
+                to={`/blog/topics/${slug}`}
+              >
+                <ListGroup.Item>
+                  {name}&nbsp;&nbsp;
+                  <Badge variant="light">{count.posts}</Badge>
+                </ListGroup.Item>
+              </LinkContainer>
+            );
+          })}
+        </ListGroup>
+      </Col>
+    </Row>
+  );
+};
+
+export default BlogTopics;
