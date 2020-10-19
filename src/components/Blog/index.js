08d2fdb8bf5d4bb1aeb997285b3ddc70e12e8214
A.N <spidernet12@gmail.com>
2020-10-19 17:40:32 +0300

    cleaned up the code and relied on withrouter to extract any required params or location data instead of passing it down from main router

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,40 +1,40 @@
 import React from "react";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import BlogPosts from "../BlogPosts/index";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import BlogPostsArchive from "../BlogPostsArchive/index";
-import BlogTopics from '../BlogTopics/index';
-import BlogNav from '../BlogNav/index';
+import BlogTopics from "../BlogTopics/index";
+import BlogNav from "../BlogNav/index";
+import BlogPostBySlug from "../BlogPostBySlug/index";
 import BlogPostsByTopic from '../BlogPostsByTopic/index';
 
 function Blog() {
-  let { path } = useRouteMatch();
-
   return (
-    <Container>
-      <Row>
-        <Col xs={12} sm={12} md={12} lg={2} xl={2} />
-        <Col>
-          <BlogNav/>
-          <Switch>
-          <Route path={`${path}/topics/:topic`}>
-              <BlogPostsByTopic />
-            </Route>
-            <Route path={`${path}/archive`}>
-              <BlogPostsArchive />
-            </Route>
-            <Route path={`${path}/topics`}>
-              <BlogTopics />
-            </Route>
-            <Route exact path={path}>
-              <BlogPosts />
-            </Route>
-          </Switch>
-        </Col>
-        <Col xs={12} sm={12} md={12} lg={2} xl={2} />
-      </Row>
-    </Container>
+    <Row>
+      <Col xs={12} sm={12} md={12} lg={2} xl={2}>
+        <BlogNav />
+      </Col>
+      <Col xs={12} sm={12} md={12} lg={10} xl={10}>
+        <Switch>
+          <Route path={`/blog/topics/:topic`}>
+            <BlogPostsByTopic />
+          </Route>
+          <Route path={`/blog/archive`}>
+            <BlogPostsArchive />
+          </Route>
+          <Route path={`/blog/topics`}>
+            <BlogTopics />
+          </Route>
+          <Route path={`/blog/posts/:slug`}>
+            <BlogPostBySlug />
+          </Route>
+          <Route exact path={`/blog`}>
+            <BlogPosts />
+          </Route>
+        </Switch>
+      </Col>
+    </Row>
   );
 }
 
