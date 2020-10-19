d8ba36651e7bfb7272b9fb1c7271d1f367907619
A.N <spidernet12@gmail.com>
2020-10-19 17:41:53 +0300

    adjusted page design and convereted the component to functional one
    ToDo: add comments section

--- src/components/BlogPostBySlug/index.js
+++ src/components/BlogPostBySlug/index.js
@@ -1,70 +1,93 @@
-import React from "react";
-import { withRouter } from "react-router";
-import { Row, Col } from 'react-bootstrap';
-import { getPostBySlug } from '../../api/blog';
-import Loading from '../Loading/index';
-class BlogPostBySlug extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      post: null
-    };
-  }
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { getPostBySlug } from "../../api/blog";
+import Loading from "../Loading/index";
+import Moment from "react-moment";
+import { TagsBuilder } from '../TagsUtility/index';
 
-  async getBlogPost(slug) {
+const BlogPostBySlug = ({ match, location, history }) => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [post, setPost] = useState(null);
+
+  // width: 100%;
+  // margin: 20px 0;
+  //   function loadComments(post: PostType) {
+  //     setEnableLoadComments(false);
+  //     (window as any).disqus_config = function () {
+  //         this.page.url = window.location.href;
+  //         this.page.identifier = post.id;
+  //     };
+
+  //     const script = document.createElement('script');
+  //     script.src = 'https://hecked-blog.disqus.com/embed.js';
+  //     script.setAttribute('data-timestamp', Date.now().toString());
+  //     document.body.appendChild(script);
+  // }
+
+  const getBlogPost = async (slug) => {
     try {
       const response = await getPostBySlug(slug);
       console.log(response);
-      this.setState({
-        error: null,
-        isLoaded: true,
-        post: {...response}
-      }, ()=>{ return response; });
+      setError(null);
+      setPost({ ...response });
     } catch (error) {
-      this.setState({
-        error: error,
-        isLoaded: true
-      }, ()=>{ return null; });
+      setError(error);
+    } finally {
+      setIsLoaded(true);
     }
+  };
+
+  useEffect(() => {
+    getBlogPost(match.params.slug);
+  }, [match.params.slug]);
+
+  if (error) {
+    return (
+      <Row>
+        <Col className="text-center">
+          <div>Error: {error.message}</div>
+        </Col>
+      </Row>
+    );
   }
 
-  async componentDidMount() {
-    const { match: { params } } = this.props;
-    await this.getBlogPost(params.slug);
+  if (!isLoaded) {
+    return (
+      <Row>
+        <Col className="text-center">
+          <Loading />
+        </Col>
+      </Row>
+    );
   }
 
-  render() {
-    if (this.state.error) {
-      return (
-        <Row>
-          <Col className="text-center">
-            <div>Error: {this.state.error.message}</div>
-          </Col>
-        </Row>
-      );
-    } else if (!this.state.isLoaded) {
-      return (
-        <Row>
-          <Col className="text-center">
-            <Loading />
-          </Col>
-        </Row>
-      );
-    } else {
-      return (
-        <Row>
-          <Col xs={12} sm={12} md={12} lg={2} xl={2}></Col>
-          <Col xs={12} sm={12} md={12} lg={8} xl={8}>
-            <div dangerouslySetInnerHTML={{__html: this.state.post.html}} />
-          </Col>
-          <Col xs={12} sm={12} md={12} lg={2} xl={2}></Col>
-        </Row>
-      );
-    }
-  }
-}
+  return (
+    <Row>
+      <Col className="mb-4">
+        <h1>
+          <b>{post.title}</b>
+        </h1>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                <Moment date={post.published_at} format="DD/MM/YYYY hh:mm A" />
+              </Tooltip>
+            }
+          >
+            <small>
+            Published  <Moment date={post.published_at} fromNow />
+            </small>
+          </OverlayTrigger><br/>
+        <small><i>Tags </i></small><TagsBuilder tags={post.tags} />
+      </Col>
+      <Col xs={12}>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Col>
+    </Row>
+  );
+};
 
 export default withRouter(BlogPostBySlug);
