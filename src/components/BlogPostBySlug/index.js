568e86057552cef08a7aa48cf19cdbe661b9f4ef
A.N <spidernet12@gmail.com>
2020-10-21 08:30:03 +0300

    added comments setion using disqus

--- src/components/BlogPostBySlug/index.js
+++ src/components/BlogPostBySlug/index.js
@@ -1,30 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import { Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Row, Col, OverlayTrigger, Tooltip, Button } from "react-bootstrap";
 import { getPostBySlug } from "../../api/blog";
 import Loading from "../Loading/index";
 import Moment from "react-moment";
-import { TagsBuilder } from '../TagsUtility/index';
+import { TagsBuilder } from "../TagsUtility/index";
+import Alert from '../Alert/index';
 
 const BlogPostBySlug = ({ match, location, history }) => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [post, setPost] = useState(null);
+  const [loadCommentsEnabled, setLoadingCommentsEnabled] = useState(true);
+  const loadComments = () => {
+    setLoadingCommentsEnabled(false);
+    window.disqus_config = function () {
+      this.page.url = window.location.href;
+      this.page.identifier = post.id;
+    };
 
-  // width: 100%;
-  // margin: 20px 0;
-  //   function loadComments(post: PostType) {
-  //     setEnableLoadComments(false);
-  //     (window as any).disqus_config = function () {
-  //         this.page.url = window.location.href;
-  //         this.page.identifier = post.id;
-  //     };
-
-  //     const script = document.createElement('script');
-  //     script.src = 'https://hecked-blog.disqus.com/embed.js';
-  //     script.setAttribute('data-timestamp', Date.now().toString());
-  //     document.body.appendChild(script);
-  // }
+    const script = document.createElement("script");
+    script.src = "https://hecked-blog.disqus.com/embed.js";
+    script.setAttribute("data-timestamp", Date.now().toString());
+    document.body.appendChild(script);
+  };
 
   const getBlogPost = async (slug) => {
     try {
@@ -47,7 +46,7 @@ const BlogPostBySlug = ({ match, location, history }) => {
     return (
       <Row>
         <Col className="text-center">
-          <div>Error: {error.message}</div>
+          <Alert title="API Error" message={error.message} type="danger" />
         </Col>
       </Row>
     );
@@ -69,22 +68,44 @@ const BlogPostBySlug = ({ match, location, history }) => {
         <h1>
           <b>{post.title}</b>
         </h1>
-          <OverlayTrigger
-            placement="top"
-            overlay={
-              <Tooltip>
-                <Moment date={post.published_at} format="DD/MM/YYYY hh:mm A" />
-              </Tooltip>
-            }
-          >
-            <small>
-            Published  <Moment date={post.published_at} fromNow />
-            </small>
-          </OverlayTrigger><br/>
-        <small><i>Tags </i></small><TagsBuilder tags={post.tags} />
+        <OverlayTrigger
+          placement="top"
+          overlay={
+            <Tooltip>
+              <Moment date={post.published_at} format="DD/MM/YYYY hh:mm A" />
+            </Tooltip>
+          }
+        >
+          <small>
+            Published <Moment date={post.published_at} fromNow />
+          </small>
+        </OverlayTrigger>
+        <br />
+        <small>
+          <i>Tags </i>
+        </small>
+        <TagsBuilder tags={post.tags} />
       </Col>
       <Col xs={12}>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Col>
+      <Col xs={12} className="mt-3 text-center">
+        {loadCommentsEnabled && (
+          <Button
+            variant="secondary"
+            onClick={() => {
+              loadComments();
+            }}
+          >
+            Show Comments
+          </Button>
+        )}
+        {!loadCommentsEnabled && (
+          <>
+            <hr />
+            <div id="disqus_thread"></div>
+          </>
+        )}
       </Col>
     </Row>
   );
