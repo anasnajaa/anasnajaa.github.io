fc5cea407e9bb0f4f998c78ff9e7fab17d5b17ad
A.N <spidernet12@gmail.com>
2020-10-21 08:30:43 +0300

    simplefied returns and cleand up the code

--- src/components/BlogPosts/index.js
+++ src/components/BlogPosts/index.js
@@ -4,15 +4,16 @@ import Pagination from "../Pagination/index";
 import BlogPostSimple from "../BlogPostSimple/index";
 import { Row, Col } from "react-bootstrap";
 import Loading from "../Loading/index";
-import { withRouter } from 'react-router-dom';
-import qs from 'qs';
+import { withRouter } from "react-router-dom";
+import qs from "qs";
+import Alert from "../Alert/index";
 
-const getQueryParams = (history)=>{
-  let params =  qs.parse(history.location.search, { ignoreQueryPrefix: true });
+const getQueryParams = (history) => {
+  let params = qs.parse(history.location.search, { ignoreQueryPrefix: true });
   return {
-    p: params.p ? parseInt(params.p) : null
-  }
-}
+    p: params.p ? parseInt(params.p) : null,
+  };
+};
 
 const BlogPosts = ({ topic, history }) => {
   const [topicTitle, setTopicTitle] = useState(null);
@@ -20,17 +21,19 @@ const BlogPosts = ({ topic, history }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [posts, setPosts] = useState([]);
   const [pagination, setPagination] = useState(null);
-  const [currentPage, setCurrentPage] = useState(getQueryParams(history).p || 1);
-  
-  const buildUrl = (page)=>{
+  const [currentPage, setCurrentPage] = useState(
+    getQueryParams(history).p || 1
+  );
+
+  const buildUrl = (page) => {
     let baseUrl = "";
-    if(topic){
-      baseUrl=`/blog/topics/${topic}`;
+    if (topic) {
+      baseUrl = `/blog/topics/${topic}`;
     } else {
-      baseUrl=`/blog`;
+      baseUrl = `/blog`;
     }
     return `${baseUrl}?p=${page}`;
-  }
+  };
 
   const getBlogPosts = async (page, topic) => {
     try {
@@ -39,8 +42,9 @@ const BlogPosts = ({ topic, history }) => {
       setPosts(response.posts);
       setPagination(response.meta.pagination);
       // grab topic title from one of the posts
-      if(topic){
-        const title = response.posts[0].tags.filter(x=>x.slug===topic)[0].name;
+      if (topic) {
+        const title = response.posts[0].tags.filter((x) => x.slug === topic)[0]
+          .name;
         setTopicTitle(title);
       }
     } catch (error) {
@@ -55,14 +59,18 @@ const BlogPosts = ({ topic, history }) => {
     setIsLoaded(false);
   };
 
-  useEffect(() => history.listen(() => {
-    const params = getQueryParams(history);
-    //const params =  qs.parse(history.location.search, { ignoreQueryPrefix: true });
-    updateCurrentPage(params.p);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }), []);
+  useEffect(
+    () =>
+      history.listen(() => {
+        const params = getQueryParams(history);
+        //const params =  qs.parse(history.location.search, { ignoreQueryPrefix: true });
+        updateCurrentPage(params.p);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }),
+    []
+  );
 
-  useEffect(()=>{
+  useEffect(() => {
     getBlogPosts(currentPage, topic);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage, topic]);
@@ -71,58 +79,66 @@ const BlogPosts = ({ topic, history }) => {
     return (
       <Row>
         <Col className="text-center">
-          <div>Error: {error.message}</div>
+          <Alert title="API Error" message={error.message} type="danger" />
         </Col>
       </Row>
     );
-  } else if (!isLoaded) {
+  }
+
+  if (!isLoaded) {
     return (
       <Row>
-        {topic &&
-        <Col xs={12}>
-          <h3><i>{topicTitle}</i></h3>
-        </Col>}
+        {topic && (
+          <Col xs={12}>
+            <h3>
+              <i>{topicTitle}</i>
+            </h3>
+          </Col>
+        )}
         <Col className="text-center">
           <Loading />
         </Col>
       </Row>
     );
-  } else {
-    return (
-      <Row>
-        {topic &&
-        <Col xs={12}>
-          <h3><i>{topicTitle}</i></h3>
-        </Col>}
-        {posts.map((post) => (
-          <Col  key={post.id} xs={12} sm={12} md={12} lg={12} xl={12}>
-            <BlogPostSimple post={post} />
-          </Col>
-        ))}
-
-        <Col xs={12} sm={12} md={4} lg={4} xl={4} />
-        <Col className="text-center" xs={12} sm={12} md={4} lg={4} xl={4}>
-          <Pagination
-            pg={pagination}
-            onNextClick={() => {
-              history.push(buildUrl(currentPage + 1));
-              //updateCurrentPage(currentPage + 1);
-            }}
-            onPreviousClick={() => {
-              history.push(buildUrl(currentPage - 1));
-              //updateCurrentPage(currentPage - 1);
-            }}
-            onPageClick={(selectPageNumber) => {
-              history.push(buildUrl(selectPageNumber));
-              //updateCurrentPage(selectPageNumber);
-            }}
-            isLoading={!isLoaded}
-          />
-        </Col>
-        <Col xs={12} sm={12} md={4} lg={4} xl={4} />
-      </Row>
-    );
   }
+
+  return (
+    <Row>
+      {topic && (
+        <Col xs={12}>
+          <h3>
+            <i>{topicTitle}</i>
+          </h3>
+        </Col>
+      )}
+      {posts.map((post) => (
+        <Col key={post.id} xs={12} sm={12} md={12} lg={12} xl={12}>
+          <BlogPostSimple post={post} />
+        </Col>
+      ))}
+
+      <Col xs={12} sm={12} md={4} lg={4} xl={4} />
+      <Col className="text-center" xs={12} sm={12} md={4} lg={4} xl={4}>
+        <Pagination
+          pg={pagination}
+          onNextClick={() => {
+            history.push(buildUrl(currentPage + 1));
+            //updateCurrentPage(currentPage + 1);
+          }}
+          onPreviousClick={() => {
+            history.push(buildUrl(currentPage - 1));
+            //updateCurrentPage(currentPage - 1);
+          }}
+          onPageClick={(selectPageNumber) => {
+            history.push(buildUrl(selectPageNumber));
+            //updateCurrentPage(selectPageNumber);
+          }}
+          isLoading={!isLoaded}
+        />
+      </Col>
+      <Col xs={12} sm={12} md={4} lg={4} xl={4} />
+    </Row>
+  );
 };
 
 export default withRouter(BlogPosts);
