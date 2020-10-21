3feea5b577f36759aafde5741250e21a0ec35da8
A.N <spidernet12@gmail.com>
2020-10-21 08:30:59 +0300

    converted to functional component

--- src/components/BlogPostsArchive/index.js
+++ src/components/BlogPostsArchive/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getArchivePosts } from "../../api/blog";
 import { Row, Col, Table, Form } from "react-bootstrap";
 import Loading from "../Loading/index";
@@ -6,38 +6,29 @@ import { v4 } from "uuid";
 import { LinkContainer } from "react-router-bootstrap";
 import ReactMoment from 'react-moment';
 import Moment from 'moment';
+import Alert from '../Alert/index';
 
-class BlogPostsArchive extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      posts: [],
-      filteredPosts: [],
-      postsElements: [],
-      searchText: "",
-    };
-  }
+const BlogPostsArchive = ()=> {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [postsElements, setPostsElements] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  async getBlogPosts() {
+  const getBlogPosts = async () => {
     try {
       const response = await getArchivePosts();
-      this.setState({
-        error: null,
-        isLoaded: true,
-        posts: [...response.posts],
-        filteredPosts: [...response.posts]
-      }, ()=>{ return response.posts; });
+      setError(null);
+      setPosts([...response.posts]);
+      return response.posts;
     } catch (error) {
-      this.setState({
-        error: error,
-        isLoaded: true
-      }, ()=>{ return []; });
+      setError(error);
+    } finally {
+      setIsLoaded(true);
     }
   }
 
-  monthYearElement(year, month) {
+  const monthYearElement = (year, month) => {
     return (
         <tr className="bg-secondary text-white" key={v4()}><td colSpan="3">
             <strong>{year}-{month}</strong>
@@ -45,7 +36,7 @@ class BlogPostsArchive extends React.Component {
     );
   }
 
-  postElement({id, published_at, slug, title}){
+  const postElement = ({id, published_at, slug, title}) => {
       return (
         <tr key={id}>
             <td>
@@ -60,87 +51,88 @@ class BlogPostsArchive extends React.Component {
       );
   }
 
-  populatePostsList() {
+  const populatePostsList = () => {
     let currentMonth = null;
     let currentYear = null;
     const elementsList = [];
 
-    this.state.posts.forEach((post) => {
+    posts.forEach((post) => {
       const datePublished = new Moment(post.published_at);
       const postMonth = datePublished.month();
       const postYear = datePublished.year();
 
-      if (post.title.toLowerCase().indexOf(this.state.searchText.toLowerCase()) === -1) return;
+      if (post.title.toLowerCase().indexOf(searchText.toLowerCase()) === -1) return;
 
       if (postMonth !== currentMonth || postYear !== currentYear) {
         currentMonth = postMonth;
         currentYear = postYear;
         elementsList.push(
-          this.monthYearElement(currentYear,  datePublished.format("MMMM"))
+          monthYearElement(currentYear,  datePublished.format("MMMM"))
         );
       }
-      elementsList.push(this.postElement(post));
+      elementsList.push(postElement(post));
     });
-
-    this.setState({
-      postsElements: [...elementsList],
-    });
+    console.log(posts);
+    setPostsElements([...elementsList]);
   }
 
-  async componentDidMount() {
-    await this.getBlogPosts();
-    this.populatePostsList();
-  }
-
-  render() {
-    if (this.state.error) {
-      return (
-        <Row>
-          <Col className="text-center">
-            <div>Error: {this.state.error.message}</div>
-          </Col>
-        </Row>
-      );
-    } else if (!this.state.isLoaded) {
-      return (
-        <Row>
-          <Col className="text-center">
-            <Loading />
-          </Col>
-        </Row>
-      );
-    } else {
-      return (
-        <Row>
-          <Col xs={12}>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control 
-              type="text" 
-              placeholder="Filter by Title" 
-              value={this.state.searchText}
-              onChange={(e)=>{
-                this.setState({
-                  searchText: e.target.value
-                }, ()=>{
-                  this.populatePostsList();
-                });
-              }}
-              />
-            </Form.Group>
-            </Form>
-          </Col>
-          <Col xs={12}>
-          <Table striped bordered hover size="sm">
-            <tbody>
-            {this.state.postsElements}
-            </tbody>
-            </Table>
-            </Col>
-        </Row>
-      );
+  useEffect(()=>{
+    const load = async () =>{
+      await getBlogPosts();
+      populatePostsList();
     }
+    load();
+  }, []);
+
+  useEffect(()=>{
+    //populatePostsList();
+  }, [searchText]);
+
+  if (error) {
+    return (
+      <Row>
+        <Col className="text-center">
+          <Alert title="API Error" message={error.message} type="danger" />
+        </Col>
+      </Row>
+    );
   }
+
+  if (!isLoaded) {
+    return (
+      <Row>
+        <Col className="text-center">
+          <Loading />
+        </Col>
+      </Row>
+    );
+  } 
+
+  return (
+    <Row>
+      <Col xs={12}>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control 
+          type="text" 
+          placeholder="Filter by Title" 
+          value={searchText}
+          onChange={(e)=>{
+            setSearchText(e.target.value);
+          }}
+          />
+        </Form.Group>
+        </Form>
+      </Col>
+      <Col xs={12}>
+      <Table striped bordered hover size="sm">
+        <tbody>
+        {postsElements}
+        </tbody>
+        </Table>
+        </Col>
+    </Row>
+  );
 }
 
 export default BlogPostsArchive;
