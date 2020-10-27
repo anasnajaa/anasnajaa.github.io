ecd7de415e94f48d88567e439f6139df16af535f
A.N <spidernet12@gmail.com>
2020-10-27 19:09:40 +0300

    landing page redsign and code cleanup

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,36 +1,111 @@
-import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { Jumbotron, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import "./style.scss";
 
 const Home = () => {
-    return (
-        <Jumbotron>
-        <h1>Hi, I'm Anas Najaa.</h1>
-        <p>
-            I'm a software developer based in Kuwait.<br/>
-            I'm currently interested in building simple to use APIs and SPAs.<br/>
-            <br/>
-            <a style={{textDecoration: "none", color:"#1DA1F2"}} className="h4" href="https://twitter.com/spidernet12">
-                <FontAwesomeIcon icon={faTwitter} />
+  return (
+    <div className="HomeComponent">
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={3} xl={3} />
+        <Col xs={12} sm={12} md={12} lg={6} xl={6}>
+          <Jumbotron className="text-center">
+            <h1>Hi, I'm Anas Najaa.</h1>
+            <p>
+              I'm a software developer based in Kuwait.
+              <br />
+              I'm currently interested in building simple to use APIs and SPAs.
+              <br />
+              <i>Learn by doing, keep learning.</i>
+            </p>
+          </Jumbotron>
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={3} xl={3} />
+      </Row>
+      <Row>
+        <Col xs={12} className="text-center">
+          <div>
+            <h2 className="mb-3">I'm avilable here</h2>
+            <a
+              className="mr-5 twitter"
+              href="https://twitter.com/spidernet12"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <FontAwesomeIcon icon={faTwitter} />
             </a>
-            <span>&nbsp;&nbsp;</span>
-            <a style={{textDecoration: "none", color:"#2867B2"}} className="h4" href="https://www.linkedin.com/in/anasnajaa">
-                <FontAwesomeIcon icon={faLinkedin} />
+            <a
+              className="mr-5 linkedin"
+              href="https://www.linkedin.com/in/anasnajaa"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
             </a>
-            <span>&nbsp;&nbsp;</span>
-            <a style={{textDecoration: "none", color:"#24292e"}} className="h4" href="https://github.com/anasnajaa">
-                <FontAwesomeIcon icon={faGithub} />
+            <a
+              className="github"
+              href="https://github.com/anasnajaa"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <FontAwesomeIcon icon={faGithub} />
             </a>
-            <span>&nbsp;&nbsp;</span>
-            <a style={{textDecoration: "none", color:"#D44638"}} className="h4" href="mailto:anas.najaa@outlook.com">
-                <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-        </p>
-        
-        </Jumbotron>
-    )
+          </div>
+        </Col>
+      </Row>
+      <footer>
+        This Website is hosted on&nbsp;
+        <a
+          href="https://pages.github.com/"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          Github Pages
+        </a>
+        &nbsp;and&nbsp;
+        <a
+          href="https://www.heroku.com/dynos"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          Heroku
+        </a>
+        <br />
+        The frontend is powered by React.js (
+        <a
+          href="https://github.com/anasnajaa/anasnajaa.github.io"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          source code
+        </a>
+        )<br />
+        API is powered by Node.js/MongoDB/Express/Postgres (
+        <a
+          href="https://github.com/KumarAbhirup/personal-website"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          source code
+        </a>
+        )<br />
+        Content is powered by&nbsp;
+        <a
+          href="https://ghost.org/docs/api/v3/content/"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          Ghost
+        </a>
+        &nbsp;acting as a headless CMS
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
