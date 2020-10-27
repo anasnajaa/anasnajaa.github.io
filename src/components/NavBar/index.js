5a891164c573093073e701d55391eef38496e38b
A.N <spidernet12@gmail.com>
2020-10-27 19:10:37 +0300

    ux improvement in mobile
    clicking on a link after expanding the humbarger menu now closes the menu automatically

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,34 +1,49 @@
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { IndexLinkContainer, LinkContainer } from "react-router-bootstrap";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import React, { useRef } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+import { useMediaQuery } from 'react-responsive';
 
-const NavigationBar = () => (
-  <Navbar className="fixed-top" bg="light" expand="lg">
-    <Container>
-      <IndexLinkContainer to="/">
+const NavigationBar = () => {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+  const navMenuToggle = useRef(null);
+
+  const handleHideMenu = () => {
+    if(isTabletOrMobile) {
+      navMenuToggle.current.dispatchEvent(
+        new MouseEvent('click', {
+          view: window,
+          bubbles: true,
+          cancelable: true,
+          buttons: 1,
+        }),
+      );
+    }
+  }
+  
+  return (
+    <Navbar className="fixed-top" bg="light" expand="lg">
+      <Container>
         <Navbar.Brand>Anas Najaa</Navbar.Brand>
-      </IndexLinkContainer>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <IndexLinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
-          </IndexLinkContainer>
-          <LinkContainer to="/blog">
-            <Nav.Link>Blog</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/services">
-            <Nav.Link>Services</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/portfolio">
-            <Nav.Link>Portfolio</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+        <Navbar.Toggle ref={navMenuToggle} aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <IndexLinkContainer to="/" onClick={handleHideMenu}>
+              <Nav.Link>Home</Nav.Link>
+            </IndexLinkContainer>
+            <LinkContainer to="/blog" onClick={handleHideMenu}>
+              <Nav.Link>Blog</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/services" onClick={handleHideMenu}>
+              <Nav.Link>Services</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/portfolio" onClick={handleHideMenu}>
+              <Nav.Link>Portfolio</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default NavigationBar;
