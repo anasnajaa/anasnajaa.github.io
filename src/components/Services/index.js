ad0e3792a3bd5327c5958c7c18004b9434c0dd32
A.N <spidernet12@gmail.com>
2020-10-27 19:11:03 +0300

    added FAQs to each service

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row, Accordion, Card, Button, Jumbotron } from "react-bootstrap";
+import { Col, Row, Accordion, Card, Button, Jumbotron, Spinner } from "react-bootstrap";
 import Loading from "../Loading/index";
 import Alert from "../Alert/index";
-import { getPageData } from "../../api/blog";
+import { getPagesByInternalTag, getPageBySlug } from "../../api/blog";
 import { parseServicePage } from '../PagesMapping/index';
+import { SimpleModal } from '../Modal/index';
 
 const Services = () => {
   
@@ -11,6 +12,11 @@ const Services = () => {
   const [headerCardContent, setHeaderCardContent] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [servicesCards, setServicesCards] = useState([]);
+
+  const [isFaqLoaded, setIsFaqLoaded] = useState(true);
+  const [showFaqModal, setShowFaqModal] = useState(false);
+  const [faqBody, setFaqBody] = useState(null);
+  const [faqTitle, setFaqTitle] = useState(null);
 
   const ServiceElement = ({card}) => {
     return (
@@ -25,16 +31,47 @@ const Services = () => {
             <div dangerouslySetInnerHTML={{ __html: card.html }} />
             <hr/>
             <Button className="mr-2" variant="primary">Place a request</Button>
-            <Button variant="success">FAQs</Button>
+            <Button 
+            variant="success"
+            disabled={!isFaqLoaded}
+            onClick={()=>{loadServicesFaqs()}}>{isFaqLoaded ? 'FAQs' : 
+            <>
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+              className="mr-2"
+            />
+            Loading...
+            </>
+            
+            }</Button>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
     )
   }
 
+  const loadServicesFaqs = async () => {
+    try {
+      setIsFaqLoaded(false);
+      const response = await getPageBySlug("services-faqs");
+      setFaqBody(response.html);
+      setFaqTitle(response.title);
+    } catch (error) {
+      setFaqTitle("Error");
+      setFaqBody(error.message);
+    } finally {
+      setIsFaqLoaded(true);
+      setShowFaqModal(true);
+    }
+  };
+
   const loadPageData = async () => {
     try {
-      const response = await getPageData("services-page");
+      const response = await getPagesByInternalTag("services-page");
       const pageContent = parseServicePage(response.pages);
       setHeaderCardContent(pageContent.headerCard);
       setServicesCards(pageContent.services);
@@ -71,6 +108,13 @@ const Services = () => {
 
   return (
     <Row>
+      <SimpleModal 
+      title={faqTitle} 
+      body={faqBody} 
+      isHtml
+      show={showFaqModal} 
+      onClose={()=>{setShowFaqModal(false)}} 
+      />
       <Col xs="12">
         <Jumbotron>
           <h1>{headerCardContent.title}</h1>
@@ -78,7 +122,6 @@ const Services = () => {
         </Jumbotron>
       </Col>
       <Col xs="12">
-        <h4>Services</h4>
           <Accordion defaultActiveKey={servicesCards[0].id}>
             {servicesCards.map((card) => {
               return <ServiceElement key={card.id} card={card} />
